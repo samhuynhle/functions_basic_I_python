5c4cc19ffbc32ed079953dc48ec1b0bab89b9088
Sam H Le <samhuynhle@gmail.com>
2019-08-03 23:20:13 -0700

    This is the initial commit into this local repository. Included was the completion of the first prediction.

---
+++ assignment.py
@@ -0,0 +1,11 @@
+#1
+def a():
+    return 5
+print(a())
+
+# Defined is function a with no defined paramaters for the function. Within function a, the code that is executed returns the value of 5.
+# Function a is called within the print method.
+# Predicted output: 5
+# Predicted output was correct.
+
+

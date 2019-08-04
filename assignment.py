b334d7d367db98da57162654d088247b7df1d5ff
Sam H Le <samhuynhle@gmail.com>
2019-08-03 23:23:41 -0700

    This is the second commit. Second prediction was completed.

--- assignment.py
+++ assignment.py
@@ -1,11 +1,22 @@
 #1
-def a():
-    return 5
-print(a())
+# def a():
+#     return 5
+# print(a())
 
-# Defined is function a with no defined paramaters for the function. Within function a, the code that is executed returns the value of 5.
+# Defined is function a, which has no defined paramaters for the function. Within function a, the code that is executed returns the value of 5.
 # Function a is called within the print method.
 # Predicted output: 5
-# Predicted output was correct.
+# Predicted output is correct.
+
+#2
+def a():
+    return 5
+print(a()+a())
+
+# Defined is function a, which has no defined paramters for the function. Within function a, the code that is execute returns a value of 5.
+# Function a is called twice within the print method. Within the print method, the values of the functions are added together. Since both values are integers,
+# The value the is printed will be the addition of both function's value.
+# Predicted output: 10
+# Predicted output is correct.
 
 

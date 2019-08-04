3ba7ea8d9bde8e19d55bcabee580820b8755758b
Sam H Le <samhuynhle@gmail.com>
2019-08-03 23:32:32 -0700

    Third commit from Sam Le into local repositoy. Completion of prediction exercises 3&4 included.

--- assignment.py
+++ assignment.py
@@ -9,14 +9,37 @@
 # Predicted output is correct.
 
 #2
-def a():
-    return 5
-print(a()+a())
+# def a():
+#     return 5
+# print(a()+a())
 
-# Defined is function a, which has no defined paramters for the function. Within function a, the code that is execute returns a value of 5.
+# Defined is function a, which has no defined parameters for the function. Within function a, the code that is execute returns a value of 5.
 # Function a is called twice within the print method. Within the print method, the values of the functions are added together. Since both values are integers,
 # The value the is printed will be the addition of both function's value.
 # Predicted output: 10
 # Predicted output is correct.
 
+#3
+# def a():
+#     return 5
+#     return 10 # A warning is given by the terminal stating this line of code is unreachable. The way this function is written, 'return 10' will never be reached as 'return 5' precedes it and ends the function.
+# print(a())
 
+# Defined is function a, which has no defined parameters for the function. Within function a, the code has two returns, the first one returning a value of 5
+# and the second returning the value of 10. When a function returns a value, the function is completed, since the first line of code for function a is 'return 5',
+# the function will be completed as the value of 5 is returned.
+# Function a is called within the print method.
+# Predicted output: 5
+# Predicted output is correct.
+
+#4
+def a():
+    return 5
+    print(10)
+print(a())
+
+# Defined is function a, which has no defined parameters for the function. Within function a, the code returns the value of 5 and prints the interger 10, but since
+# the first line of code in the function is 'return 5', the function will be completed immediately after 'return 5' is executed. 'print(10)' will not be reached 
+# and executed.
+# Function a is called within the print mehtod.
+# Predicted outcome: 5
